fea172ef5d6374c58a1a15aa287ecaa368d49955
daleondev <david@rodenheber.de>
2025-12-31 17:12:52 +0100

    ui

--- src/ui/MainWindow.hpp
+++ src/ui/MainWindow.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "CellNavigator.hpp"
+#include "pages/ConveyorPage.hpp"
 #include "pages/OverviewPage.hpp"
 #include "pages/RobotPage.hpp"
-#include "pages/ConveyorPage.hpp"
-#include "pages/SensorsPage.hpp"
 #include "pages/SafetyPage.hpp"
+#include "pages/SensorsPage.hpp"
 #include <elements.hpp>
 
 namespace tsim::ui
@@ -14,44 +14,39 @@ namespace tsim::ui
 
     class MainWindow
     {
-    public:
+      public:
         MainWindow(app& _app)
-            : m_app(_app)
-            , m_window(_app.name(), window::standard, {0, 0, 1280, 750})
-            , m_view(m_window)
-            , m_navigator([this](size_t index) { this->show_page(index); })
+          : m_app(_app)
+          , m_window(_app.name(), window::standard, { 0, 0, 1280, 750 })
+          , m_view(m_window)
+          , m_navigator([this](size_t index) { this->show_page(index); })
         {
             m_window.on_close = [this]() { m_app.stop(); };
             setup_content();
         }
 
-    private:
+      private:
         void setup_content()
         {
-            m_deck = share(deck(
-                make_overview_page(),
-                make_robot_page(),
-                make_conveyor_page(),
-                make_sensors_page(),
-                make_safety_page()
-            ));
+            // align_left_top(subject) is a proxy that returns:
+            // min = subject.min
+            // max = full_extent (IMPORTANT!)
+            // This prevents the deck (which takes the MINIMUM of its children's MAX limits)
+            // from being restricted by a small page.
 
-            auto main_layout = htile(
-                hsize(200, m_navigator.make_sidebar()),
-                hstretch(1.0, hold(m_deck))
-            );
+            m_deck = share(deck(align_left_top(make_overview_page()),
+                                align_left_top(make_robot_page()),
+                                align_left_top(make_conveyor_page()),
+                                align_left_top(make_sensors_page()),
+                                align_left_top(make_safety_page())));
 
-            auto background = box(rgba(35, 35, 37, 255));
+            auto sidebar = hsize(200, m_navigator.make_sidebar());
 
-            view_limits limits;
-            limits.min = { 1280, 750 };
-            limits.max = { full_extent, full_extent };
+            // The content area needs to stretch to fill the rest of the htile.
+            // We also wrap it in a background layer.
+            auto content_area = layer(hstretch(1.0, vstretch(1.0, hold(m_deck))), box(rgba(35, 35, 37, 255)));
 
-            // We combine limit (for window sizing) and stretch (for content distribution)
-            m_view.content(
-                limit(limits, vstretch(1.0, hstretch(1.0, main_layout))),
-                background
-            );
+            m_view.content(min_size({ 1280, 750 }, htile(sidebar, content_area)));
         }
 
         void show_page(size_t index)
