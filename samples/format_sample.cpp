454b60dc2eff64c20eec4639e8ca9d4d91453f02
daleondev <david@rodenheber.de>
2026-01-01 15:00:03 +0100

    fmt

--- samples/format_sample.cpp
+++ samples/format_sample.cpp
@@ -1,5 +1,6 @@
 #include "reflect.hpp"
 
+#include <chrono>
 #include <concepts>
 #include <format>
 #include <memory>
@@ -66,31 +67,64 @@ namespace detail
     }
 
     // clang-format off
-    template <typename T> struct is_std_utility : std::false_type {};
-    template <typename... Args> struct is_std_utility<std::tuple<Args...>> : std::true_type {};
-    template <typename T, typename U> struct is_std_utility<std::pair<T, U>> : std::true_type {};
-    template <typename T> struct is_std_utility<std::allocator<T>> : std::true_type {};
-
-    template <typename T> struct is_smart_ptr : std::false_type {};
-    template <typename T> struct is_smart_ptr<std::unique_ptr<T>> : std::true_type {};
-    template <typename T> struct is_smart_ptr<std::shared_ptr<T>> : std::true_type {};
-
-    template <typename T>
-    concept Reflectable = 
-        std::is_class_v<T> &&
-        requires { reflect::size<T>(); } &&
-        !std::ranges::range<T> &&
-        !is_std_utility<std::remove_cvref_t<T>>::value &&
-        !is_smart_ptr<std::remove_cvref_t<T>>::value;
+    template<typename T>
+    consteval auto namespace_name() noexcept -> std::string_view
+    {
+        using type_name_info = reflect::detail::type_name_info<std::remove_pointer_t<std::remove_cvref_t<T>>>;
+        constexpr std::string_view function_name{
+            reflect::detail::function_name<std::remove_pointer_t<std::remove_cvref_t<T>>>()
+        };
+        constexpr std::string_view qualified_type_name{ 
+            function_name.substr(type_name_info::begin, function_name.find(type_name_info::end) - type_name_info::begin) 
+        };
+        constexpr std::string_view tmp_type_name{ 
+            qualified_type_name.substr(0, qualified_type_name.find_first_of("<", 1)) 
+        };
+        constexpr std::string_view namespace_name{ 
+            tmp_type_name.substr(0, tmp_type_name.find_last_of("::") + 1) 
+        };
+        return namespace_name;
+    }
     // clang-format on
+
+    template<typename T>
+    consteval auto is_std_type() -> bool
+    {
+        return namespace_name<T>().starts_with("std::");
+    }
+
+    template<typename T>
+    concept Reflectable = std::is_class_v<T> && !is_std_type<T>();
+
+    template<typename T>
+    concept ScopedEnum = std::is_scoped_enum_v<T>;
 }
 
-template<typename T>
-    requires std::is_class_v<T>
+template<detail::Reflectable T>
 struct std::formatter<T>
 {
-    constexpr auto parse(std::format_parse_context& ctx) { return ctx.begin(); }
-    auto format(const T& t, auto& ctx) const
+    bool pretty{ false };
+
+    constexpr auto parse(auto& ctx) -> std::remove_reference_t<decltype(ctx)>::iterator
+    {
+        auto it{ ctx.begin() };
+        if (it == ctx.end()) {
+            return it;
+        }
+
+        if (*it == 'p') {
+            pretty = true;
+            ++it;
+        }
+
+        if (it != ctx.end() && *it != '}') {
+            throw std::format_error("Invalid format args");
+        }
+
+        return it;
+    }
+
+    auto format(const T& t, auto& ctx) const -> std::remove_reference_t<decltype(ctx)>::iterator
     {
         auto make_arg = []<typename Field>(Field&& field) -> decltype(auto) {
             using Decayed = std::remove_cvref_t<Field>;
@@ -107,35 +141,84 @@ struct std::formatter<T>
 
         return [&]<size_t... Is>(std::index_sequence<Is...>) {
             auto args{ std::forward_as_tuple(make_arg(reflect::get<Is>(t))...) };
-            return std::apply([&](const auto&... args) {
-                return std::vformat_to(ctx.out(), detail::class_format<T>(), std::make_format_args(args...));
+            return std::apply([&]<typename... Args>(Args&&... args) {
+                return std::vformat_to(
+                  ctx.out(), detail::class_format<T>(), std::make_format_args(std::forward<Args>(args)...));
             }, args);
         }(std::make_index_sequence<reflect::size<T>()>{});
     }
 };
 
-struct TestStruct
+template<detail::ScopedEnum T>
+struct std::formatter<T>
 {
-    int i;
-    float f;
-};
+    bool verbose{ false };
 
-struct TestStruct2
-{
-    TestStruct* tsp;
-    TestStruct ts;
-    double d;
-    short s;
+    constexpr auto parse(auto& ctx) -> std::remove_reference_t<decltype(ctx)>::iterator
+    {
+        auto it{ ctx.begin() };
+        if (it == ctx.end()) {
+            return it;
+        }
+
+        if (*it == 'v') {
+            verbose = true;
+            ++it;
+        }
+
+        if (it != ctx.end() && *it != '}') {
+            throw std::format_error("Invalid format args");
+        }
+
+        return it;
+    }
+
+    auto format(T t, auto& ctx) const -> std::remove_reference_t<decltype(ctx)>::iterator
+    {
+        if (verbose) {
+            return std::format_to(ctx.out(), "{}:{}", reflect::type_name(t), reflect::enum_name(t));
+        }
+        return std::format_to(ctx.out(), "{}", reflect::enum_name(t));
+    }
 };
 
 int main()
 {
-    // std::println("{}", reflect::size<TestStruct>());
-    // std::println("{}", format_size<TestStruct>());
-    // std::println("{}", std::string(format<TestStruct>()));
+    struct TestStruct
+    {
+        int i;
+        float f;
+    };
+
+    struct TestStruct2
+    {
+        TestStruct* tsp;
+        TestStruct ts;
+        double d;
+        short s;
+    };
+
+    enum class MyEnum
+    {
+        A,
+        B,
+        C
+    };
 
     TestStruct ts{};
     std::println("{}", ts);
     std::println("{}", TestStruct2{ &ts });
+    std::println("{}", MyEnum::B);
+    std::println("{:v}", MyEnum::C);
+
+    std::array<int, 3> a = { 1, 2, 3 };
+    std::println("{}", a);
+
+    std::pair p = { 3, 4uz };
+    std::println("{}", p);
+    // std::println(
+    //   "{}",
+    //   reflect::detail::function_name<std::remove_pointer_t<std::remove_cvref_t<std::pair<int, int>>>>());
+
     return 0;
 }
