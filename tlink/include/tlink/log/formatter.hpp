452bb1fa0d8729aa3b9dc894143fc40688e54a89
daleondev <david@rodenheber.de>
2026-01-02 10:37:43 +0100

    refactor

--- tlink/include/tlink/log/formatter.hpp
+++ tlink/include/tlink/log/formatter.hpp
@@ -6,7 +6,7 @@
 #include <format>
 #include <memory>
 
-namespace tlink::log::detail
+namespace tlink::log::format
 {
     // clang-format off
     template<typename T>
@@ -40,6 +40,16 @@ namespace tlink::log::detail
 
     template<typename T>
     concept ScopedEnum = std::is_scoped_enum_v<T>;
+
+    template<typename T>
+    concept VoidPtr = std::is_pointer_v<T> && std::is_void_v<std::remove_pointer_t<T>>;
+
+    template<typename T>
+    concept CharPtr =
+      std::is_pointer_v<T> && std::is_same_v<std::remove_cvref_t<std::remove_pointer_t<T>>, char>;
+
+    template<typename T>
+    concept ValuePtr = std::is_pointer_v<T> && !CharPtr<T> && !VoidPtr<T>;
 
     template<typename T>
     concept SmartPtr = requires(T p) {
@@ -103,7 +113,7 @@ namespace tlink::log::detail
         return out;
     }
 
-    static constexpr std::string_view INDENT{ "  " };
+    static constexpr std::string_view PRETTY_INDENT{ "  " };
 
     template<typename T, size_t Level = 0>
     consteval auto class_pretty_format_size() -> size_t
@@ -114,7 +124,7 @@ namespace tlink::log::detail
         reflect::for_each<T>([&size](const auto I) {
             using MemberType = decltype(typename reflect::member_type<I, T>());
 
-            size += (Level + 1) * INDENT.size();
+            size += (Level + 1) * PRETTY_INDENT.size();
             size += reflect::member_name<I, T>().size(); // "<member>"
             if constexpr (Reflectable<MemberType>) {
                 size += 2; // ": "
@@ -129,7 +139,7 @@ namespace tlink::log::detail
             size += 1; // "\n"
         });
 
-        size += Level * INDENT.size();
+        size += Level * PRETTY_INDENT.size();
         size += 3; // "}}\0"
         return size;
     }
@@ -152,7 +162,7 @@ namespace tlink::log::detail
             using MemberType = decltype(typename reflect::member_type<I, T>());
 
             for (auto i{ 0uz }; i < (Level + 1); ++i) {
-                append(INDENT);
+                append(PRETTY_INDENT);
             }
             append(reflect::member_name<I, T>());
 
@@ -171,14 +181,14 @@ namespace tlink::log::detail
         });
 
         for (auto i{ 0uz }; i < Level; ++i) {
-            append(INDENT);
+            append(PRETTY_INDENT);
         }
         append("}}\0");
         return out;
     }
 }
 
-template<tlink::log::detail::Reflectable T>
+template<tlink::log::format::Reflectable T>
 struct std::formatter<T>
 {
     bool pretty{ false };
@@ -206,21 +216,9 @@ struct std::formatter<T>
     template<typename Ctx>
     auto format(const T& t, Ctx& ctx) const -> Ctx::iterator
     {
-        auto make_arg = []<typename Field>(Field&& field) -> decltype(auto) {
-            using Type = std::remove_cvref_t<Field>;
-
-            if constexpr (std::is_same_v<Type, const char*> || std::is_same_v<Type, char*>) {
-                return std::forward<Field>(field);
-            }
-            else if constexpr (tlink::log::detail::SmartPtr<Type>) {
-                return static_cast<const void*>(field.get());
-            }
-            else if constexpr (std::is_pointer_v<Type>) {
-                return static_cast<const void*>(field);
-            }
-            else if constexpr (!std::formattable<Type, char>) {
-                static constexpr std::string_view missing{ "-" };
-                return missing;
+        auto check_arg = []<typename Field>(Field&& field) -> decltype(auto) {
+            if constexpr (!std::formattable<Field, char>) {
+                return "-";
             }
             else {
                 return std::forward<Field>(field);
@@ -234,42 +232,42 @@ struct std::formatter<T>
         };
 
         if (pretty) {
-            auto make_flat_tuple = [make_arg_fn = std::move(make_arg),
-                                    make_tuple_args_fn = std::move(make_tuple_args)]<typename Field>(
-                                     this auto&& make_flat_tuple_recursive, Field&& field) {
+            auto make_flat_tuple_args = [check_arg = std::move(check_arg),
+                                         make_tuple_args = std::move(make_tuple_args)]<typename Field>(
+                                          this auto&& make_flat_tuple_args, Field&& field) {
                 using Type = std::remove_cvref_t<Field>;
 
-                if constexpr (tlink::log::detail::Reflectable<Type>) {
+                if constexpr (tlink::log::format::Reflectable<Type>) {
                     return [&]<size_t... Is>(std::index_sequence<Is...>) {
-                        return std::tuple_cat(make_flat_tuple_recursive(reflect::get<Is>(field))...);
+                        return std::tuple_cat(make_flat_tuple_args(reflect::get<Is>(field))...);
                     }(std::make_index_sequence<reflect::size<Type>()>{});
                 }
                 else {
-                    return make_tuple_args_fn(make_arg_fn(std::forward<Field>(field)));
+                    return make_tuple_args(check_arg(std::forward<Field>(field)));
                 }
             };
 
             auto args = [&]<size_t... Is>(std::index_sequence<Is...>) {
-                return std::tuple_cat(make_flat_tuple(reflect::get<Is>(t))...);
+                return std::tuple_cat(make_flat_tuple_args(reflect::get<Is>(t))...);
             }(std::make_index_sequence<reflect::size<T>()>{});
 
-            static constexpr auto fmt{ tlink::log::detail::class_pretty_format<T>() };
+            static constexpr auto fmt{ tlink::log::format::class_pretty_format<T>() };
             return std::apply([&ctx](const auto&... args) { return std::format_to(ctx.out(), fmt, args...); },
                               args);
         }
         else {
             auto args = [&]<size_t... Is>(std::index_sequence<Is...>) {
-                return make_tuple_args(make_arg(reflect::get<Is>(t))...);
+                return make_tuple_args(check_arg(reflect::get<Is>(t))...);
             }(std::make_index_sequence<reflect::size<T>()>{});
 
-            static constexpr auto fmt{ tlink::log::detail::class_format<T>() };
+            static constexpr auto fmt{ tlink::log::format::class_format<T>() };
             return std::apply([&ctx](const auto&... args) { return std::format_to(ctx.out(), fmt, args...); },
                               args);
         }
     }
 };
 
-template<tlink::log::detail::ScopedEnum T>
+template<tlink::log::format::ScopedEnum T>
 struct std::formatter<T>
 {
     bool verbose{ false };
@@ -304,12 +302,36 @@ struct std::formatter<T>
     }
 };
 
-template<tlink::log::detail::SmartPtr T>
-struct std::formatter<T> : std::formatter<const void*>
+template<tlink::log::format::ValuePtr T>
+struct std::formatter<T>
+{
+    template<typename Ctx>
+    constexpr auto parse(Ctx& ctx) -> Ctx::iterator
+    {
+        auto it{ ctx.begin() };
+        if (it != ctx.end() && *it != '}') {
+            throw std::format_error("Invalid format args");
+        }
+        return it;
+    }
+
+    template<typename Ctx>
+    auto format(T t, Ctx& ctx) const -> Ctx::iterator
+    {
+        static constexpr auto fmt{ "{} <- |{}|" };
+        if (!t) {
+            return std::format_to(ctx.out(), fmt, "null", static_cast<const void*>(t));
+        }
+        return std::format_to(ctx.out(), fmt, *t, static_cast<const void*>(t));
+    }
+};
+
+template<tlink::log::format::SmartPtr T>
+struct std::formatter<T> : std::formatter<typename T::element_type*>
 {
     template<typename Ctx>
     auto format(const T& t, Ctx& ctx) const -> Ctx::iterator
     {
-        return std::formatter<const void*>::format(t.get(), ctx);
+        return std::formatter<typename T::element_type*>::format(t.get(), ctx);
     }
 };
