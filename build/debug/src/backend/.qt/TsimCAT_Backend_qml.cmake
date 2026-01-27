1ff35d15f89315e5a8f6882a3f2ce2c3d96d25e4
daleondev <david@rodenheber.de>
2026-01-27 22:23:23 +0100

    ignore

--- build/debug/src/backend/.qt/TsimCAT_Backend_qml.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "TsimCAT_Backend")
-set(working_dir "C:/Users/Dev.Windows-Desktop/Desktop/TwinCAT/TsimCAT/src/backend")
-set(src_and_dest_list
-
-)
-set(timestamp_file "C:/Users/Dev.Windows-Desktop/Desktop/TwinCAT/TsimCAT/build/debug/src/backend/.qt/TsimCAT_Backend_qml.txt")
--- build/debug/src/frontend/.qt/TsimCAT_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "TsimCAT")
-set(working_dir "C:/Users/Dev.Windows-Desktop/Desktop/TwinCAT/TsimCAT/src/frontend")
-set(src_and_dest_list
-
-)
-set(timestamp_file "C:/Users/Dev.Windows-Desktop/Desktop/TwinCAT/TsimCAT/build/debug/src/frontend/.qt/TsimCAT_res.txt")
